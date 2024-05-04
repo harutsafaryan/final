9c95ded0e728982232c78d52ac942bef33277f8c
Harut Safaryan <safaryanharut@gmail.com>
2024-05-04 07:48:03 +0400

    changes

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -77,82 +77,43 @@ export default function Test() {
                     <div className="hidden w-full lg:grid lg:grid-cols-7 lg:grid-rows-6 lg:gap-px">
                         {days.map((day) => (
                             <div
-                                key={day.date}
+                                key={day.date.toString()}
                                 className={classNames(
                                     day.isCurrentMonth ? 'bg-white' : 'bg-gray-50 text-gray-500',
                                     'relative px-3 py-2'
                                 )}
                             >
                                 <time
-                                    dateTime={day.date}
+                                    dateTime={day.date.toString()}
                                     className={
                                         day.isToday
                                             ? 'flex h-6 w-6 items-center justify-center rounded-full bg-indigo-600 font-semibold text-white'
                                             : undefined
                                     }
                                 >
-                                    {/* {day.date.split('-').pop().replace(/^0/, '')} */}
                                     {day.date.getDate()}
                                 </time>
-                                {day.events.length > 0 && (
-                                    <ol className="mt-2">
-                                        {day.events.slice(0, 2).map((event) => (
-                                            <li key={event.id}>
-                                                <a href={event.href} className="group flex">
+                                {day.checks.length > 0 && (
+                                    <ol className="mt-1">
+                                        {day.checks.slice(0, 2).map((check) => (
+                                            <li key={check.id}>
+                                                <a href={check.href} className="group flex">
                                                     <p className="flex-auto truncate font-medium text-gray-900 group-hover:text-indigo-600">
-                                                        {event.name}
+                                                        {check.name}
                                                     </p>
                                                     <time
-                                                        dateTime={event.datetime}
+                                                        dateTime={check.datetime}
                                                         className="ml-3 hidden flex-none text-gray-500 group-hover:text-indigo-600 xl:block"
                                                     >
-                                                        {event.time}
+                                                        {check.time}
                                                     </time>
                                                 </a>
                                             </li>
                                         ))}
-                                        {day.events.length > 2 && <li className="text-gray-500">+ {day.events.length - 2} more</li>}
+                                        {day.checks.length > 2 && <li className="text-gray-500">+ {day.checks.length - 2} more</li>}
                                     </ol>
                                 )}
                             </div>
-                        ))}
-                    </div>
-                    <div className="isolate grid w-full grid-cols-7 grid-rows-6 gap-px lg:hidden">
-                        {days.map((day) => (
-                            <button
-                                key={day.date}
-                                type="button"
-                                className={classNames(
-                                    day.isCurrentMonth ? 'bg-white' : 'bg-gray-50',
-                                    (day.isSelected || day.isToday) && 'font-semibold',
-                                    day.isSelected && 'text-white',
-                                    !day.isSelected && day.isToday && 'text-indigo-600',
-                                    !day.isSelected && day.isCurrentMonth && !day.isToday && 'text-gray-900',
-                                    !day.isSelected && !day.isCurrentMonth && !day.isToday && 'text-gray-500',
-                                    'flex h-14 flex-col px-3 py-2 hover:bg-gray-100 focus:z-10'
-                                )}
-                            >
-                                <time
-                                    dateTime={day.date}
-                                    className={classNames(
-                                        day.isSelected && 'flex h-6 w-6 items-center justify-center rounded-full',
-                                        day.isSelected && day.isToday && 'bg-indigo-600',
-                                        day.isSelected && !day.isToday && 'bg-gray-900',
-                                        'ml-auto'
-                                    )}
-                                >
-                                    {/* {day.date.split('-').pop().replace(/^0/, '')} */}
-                                    {day.date.getDate()}
-                                </time>
-                                <span className="sr-only">{day.events.length} events</span>
-                                {day.events.length > 0 && (
-                                    <span className="-mx-0.5 mt-auto flex flex-wrap-reverse">
-                                        {day.events.map((event) => (
-                                            <span key={event.id} className="mx-0.5 mb-1 h-1.5 w-1.5 rounded-full bg-gray-400" />
-                                        ))}
-                                    </span>
-                                )}
-                            </button>
                         ))}
                     </div>
                 </div>
@@ -181,17 +142,12 @@ function getDays(month: string | null, year : number, checks: []) {
     let daysCount = 30;
     const dates = [];
 
-    // console.log('checks... ', checks.find(e => e.id === 10));
-
-
-
     if (monthIndex === 0 || monthIndex === 2 || monthIndex === 4 || monthIndex === 6 || monthIndex === 7 || monthIndex === 9 || monthIndex === 11)
         daysCount = 31;
     else if (monthIndex === 1 && year % 4 === 0)
         daysCount = 29;
     else if (monthIndex === 1 && year % 4 !== 0)
         daysCount = 28;
-
 
     const startWeekDay = new Date(year, monthIndex, 1).getDay();
     const endtWeekDay = new Date(year, monthIndex, daysCount).getDay();
@@ -213,7 +169,7 @@ function getDays(month: string | null, year : number, checks: []) {
             date: new Date(year, monthIndex, i),
             isCurrentMonth: i > 0 && i <= daysCount,
             isToday: i === todayDay && monthIndex === todayMonth,
-            events: checks.filter(e => e.day === i).map(e => ({ id: e.id, name: e.todo.title }))
+            checks: checks.filter(e => e.day === i).map(e => ({ id: e.id, name: e.todo.title }))
         })
     }
 
