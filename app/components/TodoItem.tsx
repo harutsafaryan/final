25b4f4f957314f43f990b5a9a80291880e753b51
Harut Safaryan <safaryanharut@gmail.com>
2024-05-06 22:34:38 +0400

    todo changes

--- app/components/TodoItem.tsx
+++ app/components/TodoItem.tsx
@@ -1,8 +1,13 @@
 import { redirect, useFetcher, useNavigate } from "@remix-run/react";
 import { useState } from "react";
 import { GoChecklist, GoCheck } from "react-icons/go";
+import { CiEdit } from "react-icons/ci";
 import PulseLoader from "react-spinners/PulseLoader";
 import ClipLoader from "react-spinners/PulseLoader";
+
+function classNames(...classes: Array<String>) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export default function TodoItem({ todo, last, checkCount }) {
     const fetcher = useFetcher();
@@ -10,8 +15,18 @@ export default function TodoItem({ todo, last, checkCount }) {
     const isSaving = fetcher.state === 'idle';
     console.log('count: ', checkCount)
 
-    const [showRecord, setShowRecord] = useState(true);
+    const [visibility, setVisibility] = useState(false);
     const [record, setRecord] = useState("");
+
+    const handelSave = () => {
+        setRecord("");
+        setVisibility(false);
+    }
+
+    const handelVisibility = () => {
+        setRecord("");
+        setVisibility(!visibility);
+    }
 
     return (
         <div className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow-lg border-2 border-sky-800">
@@ -29,39 +44,42 @@ export default function TodoItem({ todo, last, checkCount }) {
                         </p>
                         <p>
                             <span>
-                            checks: {fetcher.state === 'idle' ? (checkCount.length === 0 ? 0 : checkCount[0]._count) : <PulseLoader size={5} color="orange" />}
+                                checks: {fetcher.state === 'idle' ? (checkCount.length === 0 ? 0 : checkCount[0]._count) : <PulseLoader size={5} color="orange" />}
                             </span>
                         </p>
                     </div>
                     <p className="mt-1 truncate text-sm text-gray-500">{todo.definition}</p>
                     <p className="mt-1 truncate text-sm text-gray-500">{todo.location}</p>
                     <p className="mt-1 truncate text-sm text-gray-500">{todo.criteria}</p>
-                    {showRecord && <input placeholder="record" className="border border-gray-800" onChange={(e) => setRecord(e.target.value)} />}
+                    <div className="flex">
+                        <CiEdit size={25} color="green" className="animate-pulse" onClick={() => setVisibility(!visibility)} />
+                        <input placeholder="enter result"
+                            className={`border border-gray-800 ${visibility ? '' : "invisible"}`}
+                            onChange={(e) => setRecord(e.target.value)} />
+                    </div>
                 </div>
                 {/* <img className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" src={todo.imageUrl} alt="" /> */}
             </div>
             <div>
                 <div className="flex flex-row divide-x divide-gray-200">
-                    <div className="flex basis-1/3">
-                        <button type="button" onClick={() => setShowRecord(!showRecord)}
-                            className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
-                        >
-                            <GoChecklist className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                            Comply
-                        </button>
-                    </div>
-                    <div className="flex basis-1/3">
-                        <button type="button" onClick={() => navigate(`/todoEdit/${todo.id}`)}>
+                    <div className="flex basis-1/2">
+                        <button type="button"
+                            className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                            onClick={() => navigate(`/todoEdit/${todo.id}`)}>
                             Edit
                         </button>
                     </div>
-                    <fetcher.Form method="post" className="flex basis-2/3 hover:bg-green-500">
+                    <fetcher.Form method="post" className="flex basis-1/2 ">
                         <input title="todoId" type="hidden" name="todoId" value={todo.id} />
-                        <input title="record" type="hidden" name="record" value={record} />
+                        <input title="record" type="hidden" name="record" value={record}
+                        />
                         <button type="submit"
-                            className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                            onClick={handelSave}
+                            className={classNames(
+                                record ? "transition duration-1000 bg-emerald-500" : "transition duration-500 bg-slate-100",
+                                "relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                            )}
                         >
-                            <GoCheck className="h-5 w-5 text-gray-400" aria-hidden="true" />
                             {isSaving ? 'Save' : 'Saving...'}
                         </button>
                     </fetcher.Form>
